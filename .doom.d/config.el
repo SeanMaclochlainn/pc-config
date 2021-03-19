f780e85ccfdab129721c2e78c3f781cca6103faa
Sean Mcloughlin <sean.mcloughlin@propylon.com>
2021-03-19 13:52:50 +0000

    added doom config files

---
+++ .doom.d/config.el
@@ -0,0 +1,43 @@
+(setq confirm-kill-emacs nil)
+(setq neo-smart-open t)
+(global-set-key "\C-s" 'swiper)
+(global-set-key (kbd "<f9>") 'save-buffer)
+(define-key global-map (kbd "C-c r") 'vr/replace)
+(define-key global-map (kbd "C-c q") 'vr/query-replace)
+;; if you use multiple-cursors, this is for you:
+(define-key global-map (kbd "C-c m") 'vr/mc-mark)
+;; to use visual-regexp-steroids's isearch instead of the built-in regexp isearch, also include the following lines:
+(define-key global-map (kbd "C-M-r") 'vr/isearch-backward) ;; C-M-r
+(define-key global-map (kbd "C-M-s") 'vr/isearch-forward) ;; C-M-s
+(setq helm-candidate-number-limit 300)
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+
+;; (use-package! elpy
+;;   :after python
+;;   :init
+;;   (advice-add 'python-mode :before 'elpy-enable)
+;;   (elpy-enable))
+
+(use-package! nswbuff                    ; Quick switching between buffers
+  :bind* (("<C-tab>"           . nswbuff-switch-to-next-buffer)
+          ("<C-S-iso-lefttab>" . nswbuff-switch-to-previous-buffer))
+  (:map nswbuff-override-map
+	("b" . nswbuff-kill-this-buffer))
+          ;;("b" nswbuff-override-map nswbuff-kill-this-buffer))
+  :config (setq nswbuff-buffer-list-function #'nswbuff-projectile-buffer-list
+                nswbuff-display-intermediate-buffers t
+                nswbuff-exclude-buffer-regexps '("^ .*" "^\\*.*\\*")))
+
+
+(defun disable-y-or-n-p (orig-fun &rest args)
+  (cl-letf (((symbol-function 'y-or-n-p) (lambda (prompt) t)))
+    (apply orig-fun args)))
+
+(advice-add 'ediff-quit :around #'disable-y-or-n-p)
+(setq magit-ediff-dwim-show-on-hunks t)
+
+(load-theme 'doom-monokai-classic t)
+(use-package! super-save
+  :config
+  (super-save-mode +1))
+(use-package! realgud-ipdb)
