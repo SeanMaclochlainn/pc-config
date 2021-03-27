237c82dc9cd0fe7265f52247cc8c4dabbcd7bf56
Sean Mcloughlin <sean.mcloughlin@propylon.com>
2021-03-27 15:46:23 +0000

    updated doom theme

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -30,7 +30,7 @@
 
 (advice-add 'ediff-quit :around #'disable-y-or-n-p)
 (setq magit-ediff-dwim-show-on-hunks t)
-(load-theme 'manoj-dark t)
+(load-theme 'doom-homage-black t)
 (use-package! super-save
   :config
   (super-save-mode +1))
