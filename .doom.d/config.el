74737b9980d587d711e2138cfb8de326be4cf41d
Sean Mcloughlin <sean.mcloughlin@propylon.com>
2021-03-27 15:14:02 +0000

    evil word search

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -45,3 +45,7 @@
        (let ((project-name (projectile-project-name)))
          (unless (string= "-" project-name)
            (format " in [%s]" project-name))))))
+(with-eval-after-load 'evil
+    (defalias #'forward-evil-word #'forward-evil-symbol)
+    ;; make evil-search-word look for symbol rather than word boundaries
+    (setq-default evil-symbol-word-search t))
