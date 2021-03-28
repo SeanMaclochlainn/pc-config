c07229a99e580d98c2ded13304e14f135aebbe6c
Sean Mcloughlin <sean.mcloughlin@propylon.com>
2021-03-28 02:43:19 +0100

    ignore vterm popup rule

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -64,3 +64,6 @@
     (defalias #'forward-evil-word #'forward-evil-symbol)
     ;; make evil-search-word look for symbol rather than word boundaries
     (setq-default evil-symbol-word-search t))
+(use-package! vterm
+  :config
+  (set-popup-rule! "^vterm" :ignore t))
