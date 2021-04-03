f9df5dea19f71993738376c0e16b72d8046de614
SeanMacLochlainn <seanmcloughlin9@gmail.com>
2021-04-03 10:48:44 +0100

    themes wip

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -12,8 +12,10 @@
 (define-key global-map (kbd "C-x C-j") 'evilnc-comment-and-kill-ring-save)
 (setq helm-candidate-number-limit 300)
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
+;; (add-to-list 'default-frame-alist '(background-color . "LightCyan3"))
+;; (add-to-list 'default-frame-alist '(cursor-color . "palegoldenrod"))
 (setq org-startup-folded t)
-
+;; (disable-theme)
 (use-package! magit
   :config
   (define-key magit-section-mode-map (kbd "<C-tab>") nil) ;; conflicts with nswbuff
@@ -37,17 +39,41 @@
 (advice-add 'ediff-quit :around #'disable-y-or-n-p)
 (setq magit-ediff-dwim-show-on-hunks t)
 (setq +evil-want-o/O-to-continue-comments nil)
+;; (add-hook 'after-init-hook
+;;           (lambda () (run-with-timer 5 nil #'set-cursor-color "SystemRedColor")))
+;; (setq zenburn-scale-org-headlines t)
+;; (disable-theme 'doom-one)
+;; (setq zenburn-override-colors-alist
+;;       '(
+;; ;;         ;; ("zenburn-bg+3" . "#425af5")) ;; line nos
+;; ;;         ("zenburn-bg-08" . "#000000") ;; background
+;; ;;         ;; ("zenburn-bg" . "#425af5")) ;;current line
+;;         ("zenburn-bg-1"     . "#0739b8")) ;;mark highlighter
+;;       )
+;; (load-theme 'zenburn t)
+;; (load-theme 'solarized-light t)
+;; (load-theme 'manoj-dark)
+;; (set-face-attribute 'hl-line nil :inherit nil :background "gray6")
 
-(setq zenburn-scale-org-headlines t)
-(setq zenburn-override-colors-alist
-      '(
-;;         ;; ("zenburn-bg+3" . "#425af5")) ;; line nos
-;;         ("zenburn-bg-08" . "#000000") ;; background
-;;         ;; ("zenburn-bg" . "#425af5")) ;;current line
-        ("zenburn-bg-1"     . "#0739b8")) ;;mark highlighter
-      )
-(load-theme 'zenburn t)
+;; (set-face-attribute 'hl-line nil)
+;; (load-theme 'dichromacy)
 
+;; (set-face-attribute 'hl-line nil :inherit nil)
+
+;; (set-face-attribute 'hl-line nil :inherit nil :background "gray6")
+;; (global-hl-line-mode 1)
+
+;; (custom-set-faces!
+;;   '(hl-line nil :inherit nil :background "gray6")
+;; )
+
+;; (global-hl-line-mode 1)
+;; (set-face-attribute 'highlight nil :background "#3e4446" :foreground 'unspecified)
+
+;; (set-face-attribute 'region nil :background "#8fc8db" :foreground 'unspecified)
+;; (set-cursor-color "#ffffff")
+;; (set-face-attribute 'mouse nil :background "#d9cc59" :foreground 'unspecified)
+;; (load-theme "default")
 (use-package! super-save
   :config
   (super-save-mode +1))
@@ -112,3 +138,36 @@
 (map! :after evil-org
 :map evil-org-mode-map
 :i "C-h" #'backward-delete-char-untabify)
+
+;; (use-package! eaf
+;;   :load-path "~/.emacs.d/site-lisp/emacs-application-framework" ; Set to "/usr/share/emacs/site-lisp/eaf" if installed from AUR
+;;   :init
+;;   (use-package! epc :defer t :ensure t)
+;;   (use-package! ctable :defer t :ensure t)
+;;   (use-package! deferred :defer t :ensure t)
+;;   (use-package! s :defer t :ensure t)
+;;   :custom
+;;   (eaf-browser-continue-where-left-off t)
+;;   :config
+;;   (eaf-setq eaf-browser-enable-adblocker "true")
+;;   (eaf-bind-key scroll_up "C-n" eaf-pdf-viewer-keybinding)
+;;   (eaf-bind-key scroll_down "C-p" eaf-pdf-viewer-keybinding)
+;;   (eaf-bind-key take_photo "p" eaf-camera-keybinding)
+;;   (eaf-bind-key nil "M-q" eaf-browser-keybinding)) ;; unbind, see more in the Wiki
+;; (require 'eaf-evil)
+
+;; (define-key key-translation-map (kbd "SPC")
+;;     (lambda (prompt)
+;;       (if (derived-mode-p 'eaf-mode)
+;;           (pcase eaf--buffer-app-name
+;;             ("browser" (if  (string= (eaf-call-sync "call_function" eaf--buffer-id "is_focus") "True")
+;;                            (kbd "SPC")
+;;                          (kbd eaf-evil-leader-key)))
+;;             ("pdf-viewer" (kbd eaf-evil-leader-key))
+;;             ("image-viewer" (kbd eaf-evil-leader-key))
+;;             (_  (kbd "SPC")))
+;;         (kbd "SPC"))))
+;; (dolist (theme custom-enabled-themes)
+;;   (disable-theme theme))
+;; (disable-theme 'doom-one)
+;; (disable-theme 'solaire-swap-bg-theme)
