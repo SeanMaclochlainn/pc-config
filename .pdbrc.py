9f2e975c98012bab7af4d7fe14ca26b1932f37a1
sean <seanmcloughlin9@gmail.com>
2021-05-05 20:20:10 +0100

    updated .pdbrc.py

--- .pdbrc.py
+++ .pdbrc.py
@@ -2,3 +2,6 @@ import pdb
 
 class Config(pdb.DefaultConfig):
     sticky_by_default = True
+    # current_line_color = "39;49;7"
+    # current_line_color = "7;7;7"
+    current_line_color = "39;49;20"
