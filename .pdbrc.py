1dbc9dbde4fc70b984855f4de1a52c0ef4e7f420
Sean <seanmcloughlin9@gmail.com>
2021-06-13 10:36:30 +0100

    pdbrc update

--- .pdbrc.py
+++ .pdbrc.py
@@ -2,6 +2,21 @@ import pdb
 
 class Config(pdb.DefaultConfig):
     sticky_by_default = True
-    # current_line_color = "39;49;7"
-    # current_line_color = "7;7;7"
     current_line_color = "39;49;20"
+
+    def setup(self, pdb):
+        # Save history across sessions
+        import readline
+        from pathlib import Path
+        histfile = Path("~/.pdb-pyhist").expanduser()
+        try:
+            readline.read_history_file(histfile)
+        except IOError:
+            pass
+        import atexit
+        atexit.register(readline.write_history_file, histfile)
+        readline.set_history_length(500)
+
+#don't think this is needed
+# _pdbrc_init()
+# del _pdbrc_init
