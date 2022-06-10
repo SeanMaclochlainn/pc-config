a75844e442de1e89faefcdebb09047ef9e9add86
SeanMacLochlainn <seanmcloughlin9@gmail.com>
2022-06-10 23:20:59 +0100

    Update pdb files

--- .pdbrc.py
+++ .pdbrc.py
@@ -1,20 +1,37 @@
 import pdb
+import readline
+from pathlib import Path
+import atexit
+
+
+def clean_history_entries(history_file_path):
+    with open(history_file_path, "r") as history_file:
+        lines = history_file.readlines()
+
+    # exclude q,c and ll entries
+    new_lines = [line for line in lines if line not in ("q\n", "c\n", "ll\n")]
+
+    with open(history_file_path, "w") as history_file:
+        history_file.writelines(new_lines)
+
 
 class Config(pdb.DefaultConfig):
     sticky_by_default = True
     current_line_color = "39;49;20"
+    highlight = True
+    show_traceback_on_error = True
 
     def setup(self, pdb):
-        # Save history across sessions
-        import readline
-        from pathlib import Path
+        gdrive_history_path = Path(
+            "~/gdrive/programming/python/.pdb-pyhist"
+        ).expanduser()
+        if gdrive_history_path.exists():
+            histfile = gdrive_history_path
+        else:
+            histfile = Path("~/.pdb-pyhist").expanduser()
+        readline.read_history_file(histfile)
 
-        histfile = Path("~/.pdb-pyhist").expanduser()
-        try:
-            readline.read_history_file(histfile)
-        except IOError:
-            pass
-        import atexit
-
+        atexit.register(clean_history_entries, histfile)
         atexit.register(readline.write_history_file, histfile)
-        readline.set_history_length(500)
+
+        readline.set_history_length(10000)
